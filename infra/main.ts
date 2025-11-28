bd4b48e23114f5de32501a8c440039a01244fea1
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-29 05:51:18 +1100

    initialising new project

---
+++ infra/main.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import { App, StackProps } from 'aws-cdk-lib';
+import { CognitoKitStack } from './stacks/cognito-kit/stack';
+
+const {
+    CDK_DEPLOY_ACCOUNT = process.env.CDK_DEFAULT_ACCOUNT,
+    CDK_DEPLOY_REGION = process.env.CDK_DEFAULT_REGION,
+    APP_NAME = 'CognitoKit',
+    STAGE = process.env.NODE_ENV || 'staging',
+} = process.env;
+
+const baseProps: StackProps = {
+    env: {
+        account: CDK_DEPLOY_ACCOUNT,
+        region: CDK_DEPLOY_REGION,
+    },
+};
+
+const app = new App();
+
+new CognitoKitStack(app, 'CognitoKitStack', {
+    ...baseProps,
+    appName: APP_NAME,
+    stage: STAGE,
+});
---
+++ infra/stacks/cognito-kit/stack.ts
@@ -0,0 +1,17 @@
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export type CognitoKitStackProps = StackProps & {
+    appName: string;
+    stage: string;
+};
+
+export class CognitoKitStack extends Stack {
+    public readonly apiUrl: string;
+
+    constructor(scope: Construct, id: string, props: CognitoKitStackProps) {
+        super(scope, id, props);
+
+        const { appName, stage } = props;
+    }
+}

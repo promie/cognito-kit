c710054555b2dc688bff631833cc9ee1bdd880ba
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-29 08:24:17 +1100

    added construct to the main stack with cfn outputs

--- infra/stacks/cognito-kit/stack.ts
+++ infra/stacks/cognito-kit/stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { CognitoConstruct } from './constructs/auth/cognito.construct'
 import { UserTableConstruct } from './constructs/storage/userTable.construct'
 
 export type CognitoKitStackProps = StackProps & {
@@ -15,11 +16,19 @@ export class CognitoKitStack extends Stack {
 
     const { appName, stage } = props
 
+    // DynamoDB Table for user data
     const userTable = new UserTableConstruct(this, 'UserTable', {
       appName,
       stage,
     })
 
+    // Cognito User Pool for authentication
+    const cognito = new CognitoConstruct(this, 'Cognito', {
+      appName,
+      stage,
+    })
+
+    // DynamoDB Outputs
     new CfnOutput(this, 'UserTableName', {
       value: userTable.table.tableName,
       description: 'DynamoDB table name for user data',
@@ -30,6 +39,25 @@ export class CognitoKitStack extends Stack {
       value: userTable.table.tableArn,
       description: 'DynamoDB table ARN for user data',
       exportName: `${appName}-${stage}-UserTableArn`,
+    })
+
+    // Cognito Outputs
+    new CfnOutput(this, 'UserPoolId', {
+      value: cognito.userPool.userPoolId,
+      description: 'Cognito User Pool ID',
+      exportName: `${appName}-${stage}-UserPoolId`,
+    })
+
+    new CfnOutput(this, 'UserPoolClientId', {
+      value: cognito.userPoolClient.userPoolClientId,
+      description: 'Cognito User Pool Client ID',
+      exportName: `${appName}-${stage}-UserPoolClientId`,
+    })
+
+    new CfnOutput(this, 'UserPoolArn', {
+      value: cognito.userPool.userPoolArn,
+      description: 'Cognito User Pool ARN',
+      exportName: `${appName}-${stage}-UserPoolArn`,
     })
   }
 }

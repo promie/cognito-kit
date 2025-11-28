8e663c9127b8284b40d575d980d9dd3f99dc4ae1
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-29 07:36:28 +1100

    added ESlint and prettier to project

---
+++ run.sh
@@ -0,0 +1,6 @@
+# Initiate new CDK project
+npx cdk init app --language typescript
+
+
+# Install prettier and lint packages
+npm i -D @eslint/js @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier prettier typescript-eslint

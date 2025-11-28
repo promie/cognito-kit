07ef6c8ee474217a3567b5883b877b99a227df49
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-29 08:14:18 +1100

    provision a new user table dynamo db construct

--- infra/stacks/cognito-kit/stack.ts
+++ infra/stacks/cognito-kit/stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { UserTableConstruct } from './constructs/storage/userTable.construct'
 
 export type CognitoKitStackProps = StackProps & {
   appName: string
@@ -13,5 +14,22 @@ export class CognitoKitStack extends Stack {
     super(scope, id, props)
 
     const { appName, stage } = props
+
+    const userTable = new UserTableConstruct(this, 'UserTable', {
+      appName,
+      stage,
+    })
+
+    new CfnOutput(this, 'UserTableName', {
+      value: userTable.table.tableName,
+      description: 'DynamoDB table name for user data',
+      exportName: `${appName}-${stage}-UserTableName`,
+    })
+
+    new CfnOutput(this, 'UserTableArn', {
+      value: userTable.table.tableArn,
+      description: 'DynamoDB table ARN for user data',
+      exportName: `${appName}-${stage}-UserTableArn`,
+    })
   }
 }

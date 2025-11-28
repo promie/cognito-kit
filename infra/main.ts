80a971b9f9b101a98e818876b62ea547fad43de3
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-29 07:55:59 +1100

    common infra utils and lambda version upgrade to node 24

--- infra/main.ts
+++ infra/main.ts
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import { App, StackProps } from 'aws-cdk-lib';
-import { CognitoKitStack } from './stacks/cognito-kit/stack';
+import 'source-map-support/register'
+import { App, StackProps } from 'aws-cdk-lib'
+import { CognitoKitStack } from './stacks/cognito-kit/stack'
 
 const {
-    CDK_DEPLOY_ACCOUNT = process.env.CDK_DEFAULT_ACCOUNT,
-    CDK_DEPLOY_REGION = process.env.CDK_DEFAULT_REGION,
-    APP_NAME = 'CognitoKit',
-    STAGE = process.env.NODE_ENV || 'staging',
-} = process.env;
+  CDK_DEPLOY_ACCOUNT = process.env.CDK_DEFAULT_ACCOUNT,
+  CDK_DEPLOY_REGION = process.env.CDK_DEFAULT_REGION,
+  APP_NAME = 'CognitoKit',
+  STAGE = process.env.NODE_ENV || 'staging',
+} = process.env
 
 const baseProps: StackProps = {
-    env: {
-        account: CDK_DEPLOY_ACCOUNT,
-        region: CDK_DEPLOY_REGION,
-    },
-};
+  env: {
+    account: CDK_DEPLOY_ACCOUNT,
+    region: CDK_DEPLOY_REGION,
+  },
+}
 
-const app = new App();
+const app = new App()
 
 new CognitoKitStack(app, 'CognitoKitStack', {
-    ...baseProps,
-    appName: APP_NAME,
-    stage: STAGE,
-});
+  ...baseProps,
+  appName: APP_NAME,
+  stage: STAGE,
+})

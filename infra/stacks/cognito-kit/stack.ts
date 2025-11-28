d451b8a4c2a2dc3e2de0ff18906cd2c6f5e6ea3f
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-29 07:42:29 +1100

    apply prettier across project

--- infra/stacks/cognito-kit/stack.ts
+++ infra/stacks/cognito-kit/stack.ts
@@ -1,17 +1,17 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 
 export type CognitoKitStackProps = StackProps & {
-    appName: string;
-    stage: string;
-};
+  appName: string
+  stage: string
+}
 
 export class CognitoKitStack extends Stack {
-    public readonly apiUrl: string;
+  public readonly apiUrl: string
 
-    constructor(scope: Construct, id: string, props: CognitoKitStackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props: CognitoKitStackProps) {
+    super(scope, id, props)
 
-        const { appName, stage } = props;
-    }
+    const { appName, stage } = props
+  }
 }
